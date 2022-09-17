f448f88399441755414b7cb84340634de32f836e
DevSammy22 <sadedeji1992@yahoo.com>
2022-09-17 22:56:07 +0100

    Add project files.

---
+++ Domain/Models/Account.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Models.Commons;
+
+namespace Domain.Models
+{
+    public class Account
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string AccountName { get; set; }
+        public string Email { get; set; }
+        public decimal CurrentAccountBalance { get; set; }
+        public AccountType AccountType { get; set; }
+        public string AccountNumberGenerated { get; set; }
+        public byte[] PinHash { get; set; }
+        public byte[] PinSalt { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateLastUpdated { get; set; }
+
+
+        Random random = new Random();
+        public Account()
+        {
+            AccountNumberGenerated = Convert.ToString((long)Math.Floor(random.NextDouble() * 9_000_000_000L + 1_000_000_000L));
+        }
+    }
+}
